9b61a3fd8c2581513bf3114be059e26c21bf7f28
Krishnendu Nandy <krishnendunandy6@gmail.com>
2022-05-06 09:12:22 +0530

    branch-1 #comment shop and authorjs

--- first-react-app/src/Custom_Components/Blogs/Author.js
+++ first-react-app/src/Custom_Components/Blogs/Author.js
@@ -1,13 +1,25 @@
 import React from "react";
 
+function CreateList(props){
+    const getAuthorName = props.authorName;
+    const listItems = getAuthorName.map((name) => <li>{name}</li>)
+    return(
+        <>
+            <ul>
+                {listItems}
+            </ul>
+        </>
+    );
+}
+
 export default function Author(){
- 
+    const Authors = ["Ram", "Shyam", "Jaddu"];
     return(
         <>
         <div className="App">
             <header className="App-header">
-                <p>No Author Data<br/>
-                </p>
+                <p>List of Authors</p>
+                    {<CreateList authorName={Authors}/>}
             </header>
         </div>
         </>
--- first-react-app/src/Custom_Components/Shop.js
+++ first-react-app/src/Custom_Components/Shop.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState }  from "react";
+// import { Button } from "react-bootstrap";
 
 export default function Shop(){
+
+    const [name, setName] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`The name you entered was: ${name}`);
+    }
  
     return(
         <>
         <div className="App">
             <header className="App-header">
-                <p>Fun Shopping comming soon...<br/>
-                </p>
+                <p>Register Yourself First</p><br/>
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <label className="lable-for-form">Enter your name:</label><br/>
+                        <input type="text" className="form-input-field" value={name} onChange={(e) => setName(e.target.value)}/><br/>
+                        <input type="submit" />
+                        {/* <Button variant="primary" onClick={handleSubmit}>Submit</Button> */}
+                    </form>
+                </div>
             </header>
         </div>
         </>
     );
     
 }
+
+// ReactDOM.render(<MyForm />, document.getElementById('root'));
